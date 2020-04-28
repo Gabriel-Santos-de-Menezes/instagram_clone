f5b5aee74ed866a9e57630e7fcc54452379501c6
Gabriel Santos <gabriel.santos.m112@gmail.com>
2020-04-28 14:22:25 -0300

    Criando a função ajax para ,amipular a exibição de curtidas na galeria do perfil

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -56,4 +56,36 @@
          }
      };
      xmlreq.send(null);
+     
+ }
+
+ function mosta_curtidas(){
+     // Declaração de Variáveis
+    var classe = document.getElementById('post_img_perfil').className;
+    classe = "mostar_curtidas";
+    console.log (classe);
+     var xmlreq = CriaRequest();
+      
+     // Exibi a imagem de progresso
+     //result.innerHTML = '<img src="https://media.giphy.com/media/N256GFy1u6M6Y/giphy.gif">';
+      
+     // Iniciar uma requisição
+     xmlreq.open("GET", "/perfil", true);
+     
+      
+     // Atribui uma função para ser executada sempre que houver uma mudança de ado
+     xmlreq.onreadystatechange = function(){
+          
+         // Verifica se foi concluído com sucesso e a conexão fechada (readyState=4)
+         if (xmlreq.readyState == 4) {
+              
+             // Verifica se o arquivo foi encontrado com sucesso
+             if (xmlreq.status == 200) {
+                 result.innerHTML = xmlreq.responseText;
+             }else{
+                 result.innerHTML = "Erro: " + xmlreq.statusText;
+             }
+         }
+     };
+     xmlreq.send(null);
  }
