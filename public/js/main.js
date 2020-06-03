6124e866094f64c220496acbdea199ded5009c5e
Gabriel Santos <gabriel.santos.m112@gmail.com>
2020-06-03 00:48:29 -0300

    Adiciona blibioteca de emoji para ser utilizado no direct do usuário

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,9 @@
-
+function enviar(){
+    document.getElementById('enviar').innerHTML = "Enviar";
+}
                 
+window.emojiPicker = new EmojiPicker({ assetsPath: 'lib/images/', popupButtonClasses: 'fa fa-smile-o' }); window.emojiPicker.discover();
+
 
 
 
